d74a02bacf26ce66731b2a8c1f70009336e8f3bb
ornicar <thibault.duplessis@gmail.com>
2011-07-20 20:03:54 -0700

    Rename hydrate to unserialize in AlbumDao

--- src/main/scala/furet/dao/AlbumDao.scala
+++ src/main/scala/furet/dao/AlbumDao.scala
@@ -3,15 +3,15 @@ package furet.dao
 import furet.model._
 import com.novus.salat._
 import com.novus.salat.global._
-import com.novus.salat.annotations._
 import com.mongodb.casbah.Imports._
-import com.novus.salat.dao.SalatDAO
 
 class AlbumDao {
   val collection = MongoConnection("localhost")("furet")("album");
-  def findAll: Array[Album] = collection.toArray map hydrate
+
+  def findAll: Array[Album] = collection.toArray map unserialize
   def drop = collection remove MongoDBObject.empty
   def save(album: Album) = collection insert serialize(album)
-  def hydrate(dbo: DBObject): Album = grater[Album] asObject dbo
+
+  def unserialize(dbo: DBObject): Album = grater[Album] asObject dbo
   def serialize(album: Album): DBObject = grater[Album] asDBObject album
 }
